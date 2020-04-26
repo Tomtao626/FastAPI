4914cbef123013dc017d3d800eb98214206c673f
tom <tom@autobondinc.com>
2020-04-26 11:42:07 +0800

    dddd

--- demo01.py
+++ demo01.py
@@ -16,5 +16,9 @@ app = FastAPI()
 async def main():
     return {"msg":"HelloWorld,FastAPI"}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id:int, q:str=None):
+    return {"item_id":item_id,"q":q}
+
 if __name__ == '__main__':
     uvicorn.run(app, host="127.0.0.1", port=8000, debug=True)
